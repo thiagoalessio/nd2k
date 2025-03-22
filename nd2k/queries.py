1f085d18b2c41a39ec348a83f9c9926da41b0461
thiagoalessio <thiagoalessio@me.com>
2025-03-22 21:45:21 +0100

    Extract method "is_a_purchase"

--- nd2k/queries.py
+++ nd2k/queries.py
@@ -40,7 +40,7 @@ def fits_as_trading_fee(op: Operation, tr: Trade) -> bool:
 
 	any_asset = get_any_asset(tr.operations)
 
-	if any_asset.type.name == "BUY":
+	if is_a_purchase(tr):
 		return op.symbol == tr.trading_pair.base
 
 	if any_asset.type.name == "SELL":
@@ -51,6 +51,11 @@ def fits_as_trading_fee(op: Operation, tr: Trade) -> bool:
 
 def is_trading_fee(op: Operation) -> bool:
 	return op.type.name == "TRADING_FEE"
+
+
+def is_a_purchase(tr: Trade) -> bool:
+	any_asset = get_any_asset(tr.operations)
+	return any_asset.type.name == "BUY"
 
 
 def get_any_asset(ops: TradeOperations) -> Operation:
