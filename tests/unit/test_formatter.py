f2891dadcc005d72b43aecf5742202372327cb84
thiagoalessio <thiagoalessio@me.com>
2025-03-28 13:02:02 +0100

    Remove useless unit tests for date conversion
    That's basically testing Python's datetime, and the expected format is
    covered by functional tests.

--- tests/unit/test_formatter.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from nd2k import formatter
-
-
-def test_format_date() -> None:
-	date = datetime(1970, 3, 15, 23, 45, 56)
-	assert formatter.format_date(date) == "1970-03-15 23:45:56"
