5a0d2494ec584b37cd49a92667a8613fa94477f9
thiagoalessio <thiagoalessio@me.com>
2025-04-07 23:21:09 +0200

    Clean up swaps file

--- nd2k/swap.py
+++ nd2k/swap.py
@@ -9,50 +9,43 @@ class Swap(Transaction):
 	asset_a: Operation
 	asset_b: Operation
 
+
 	@property
 	def date(self) -> datetime:
 		return self.asset_a.date
+
 
 	@property
 	def group_index(self) -> str:
 		return "".join([
 			str(self.date),
-			self.summary,
-			self.asset_a.symbol,
-			self.asset_b.symbol
-		])
-
-	@property
-	def summary(self) -> str:
-		return "".join([
 			self.asset_a.summary,
 			self.asset_a.symbol,
-			"/",
 			self.asset_b.symbol
 		])
 
+
 	def format(self) -> list[str]:
-		a = self.asset_a
-		b = self.asset_b
 		return [
-			self.formatted_date, # Date
-			f"{a.amount}",       # Sent Amount
-			f"{a.symbol}",       # Sent Currency
-			f"{b.amount}",       # Received Amount
-			f"{b.symbol}",       # Received Currency
-			"",                  # Fee Amount
-			"",                  # Fee Currency
-			"",                  # Net Worth Amount
-			"",                  # Net Worth Currency
-			"swap",              # Label
-			a.summary,           # Description
-			"",                  # TxHash
+			self.formatted_date,      # Date
+			f"{self.asset_a.amount}", # Sent Amount
+			f"{self.asset_a.symbol}", # Sent Currency
+			f"{self.asset_b.amount}", # Received Amount
+			f"{self.asset_b.symbol}", # Received Currency
+			"",                       # Fee Amount
+			"",                       # Fee Currency
+			"",                       # Net Worth Amount
+			"",                       # Net Worth Currency
+			"swap",                   # Label
+			self.asset_a.summary,     # Description
+			"",                       # TxHash
 		]
 
 
 class PartialSwap:
 	def __init__(self, asset_a: Operation):
 		self.asset_a = asset_a
+
 
 	def complete(self, asset_b: Operation) -> Swap:
 		return Swap(asset_a=self.asset_a, asset_b=asset_b)
