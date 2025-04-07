dceafe695f473fffd47bbea53e6732ecd0724c56
thiagoalessio <thiagoalessio@me.com>
2025-04-07 13:53:17 +0200

    Simplify formatter.universal

--- nd2k/formatter.py
+++ nd2k/formatter.py
@@ -1,41 +1,21 @@
 from .types import Transaction, CSV
-from .trade import Trade
-from .swap import Swap
-from .exchange import Exchange
-
-
-universal_csv_headers = [
-	"Date",
-	"Sent Amount",
-	"Sent Currency",
-	"Received Amount",
-	"Received Currency",
-	"Fee Amount",
-	"Fee Currency",
-	"Net Worth Amount",
-	"Net Worth Currency",
-	"Label",
-	"Description",
-]
 
 
 def universal(transactions: list[Transaction]) -> CSV:
 	rows = []
-	rows.append(universal_csv_headers)
-
+	rows.append([ # headers
+		"Date",
+		"Sent Amount",
+		"Sent Currency",
+		"Received Amount",
+		"Received Currency",
+		"Fee Amount",
+		"Fee Currency",
+		"Net Worth Amount",
+		"Net Worth Currency",
+		"Label",
+		"Description",
+	])
 	for t in transactions:
-		if isinstance(t, Trade):
-			rows.append(t.format())
-			continue
-
-		if isinstance(t, Exchange):
-			rows.append(t.format())
-			continue
-
-		if isinstance(t, Swap):
-			rows.append(t.format())
-			continue
-
 		rows.append(t.format())
-
 	return rows
